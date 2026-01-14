5ba0c67ba55f34b97e268fbd7abc185659954c68
Your Name <your.email@example.com>
2026-01-14 01:05:06 -0500

    Update app.py and index.html

--- my_app/app.py
+++ my_app/app.py
@@ -9,6 +9,17 @@ def hello_world():
     # This looks inside the 'templates' folder for index.html
     return render_template('index.html')
 
+# 'About Me ' Page
+@app.route('/About Me')
+def about_me():
+    return "<h1>About Me Page</h1> Information about me will go here.</h1>"
+# 'Gallery' Page
+@app.route('/Gallery')
+def gallery():
+    return "<h1>Gallery Page</h1> A collection of my photographs will go here.</h1>"
+@app.route('/Inquire')
+def Inquire():
+    return "<h1>Contact Page</h1> Information on how to reach me will go here.</h1>"
 if __name__ == '__main__':
     # Start the server in debug mode
     app.run(debug=True)
